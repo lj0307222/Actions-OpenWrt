54f0198ebb2b3f270d64b0864914bbe2295e1c45
lj0307222 <61595433+lj0307222@users.noreply.github.com>
2021-04-12 20:00:35 +0800

    Update diy-part1.sh
    yy

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i 's/src-git packages https://github.com/coolsnowwolf/packages/src-git lienol https://github.com/Lienol/openwrt-package/' feeds.conf.default
+#sed -i 's/src-git packages https://github.com/coolsnowwolf/packages/src-git lienol https://github.com/Lienol/openwrt-package/' feeds.conf.default
+sed -i '$a src/gz openwrt_custom https://op.supes.top/packages/x64' /etc/opkg/distfeeds.conf
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
